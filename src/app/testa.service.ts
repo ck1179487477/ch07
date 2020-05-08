f2ad816c1f6bba67274fc9588dfedc19847511dd
陈昆 <1179487477@qq.com>
2020-05-08 09:55:03 +0800

    Signed-off-by: 陈昆 <1179487477@qq.com>

---
+++ src/app/testa.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from "@angular/core";
+import { TestBService } from './testb.service';
+
+@Injectable(
+    {
+        providedIn: 'root'
+    }
+)
+export class TestAService {
+    title: string;
+
+    constructor(private bService: TestBService) {
+        this.title = 'Hello,A';
+    }
+
+    getTitle() {
+        return this.title;
+    }
+
+    setTitle(t: string) {
+        this.title = t;
+    }
+}
